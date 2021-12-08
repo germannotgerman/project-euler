61cb63017eecc3d71b7b33c0d63b0edb97dc6e5a
German Mumma <german.mumma@merada.ee>
2021-12-08 09:26:33 +0200

    refactor next fibonacci number calculation

--- solutions/even_fibonacci_numbers.py
+++ solutions/even_fibonacci_numbers.py
@@ -8,20 +8,14 @@ class EvenFibonaccyNumbers:
         self.even_fibonacci_terms = [2]
         
     def get_terms_below(self, limit):
-        length_of_fibonacci_numbers = len(self.fibonacci_terms)
-        f_n_minus_one = self.fibonacci_terms[length_of_fibonacci_numbers-1]
-        f_n_minus_two = self.fibonacci_terms[length_of_fibonacci_numbers-2]
-        next_fibonacci_number = f_n_minus_one + f_n_minus_two
+        next_fibonacci_number = self.get_next_fibonacci_number()
         if(next_fibonacci_number < limit):
             self.fibonacci_terms.append(next_fibonacci_number)
             self.get_terms_below(limit)
         return self.fibonacci_terms
     
     def get_even_terms_below(self, limit):
-        length_of_fibonacci_numbers = len(self.fibonacci_terms)
-        f_n_minus_one = self.fibonacci_terms[length_of_fibonacci_numbers-1]
-        f_n_minus_two = self.fibonacci_terms[length_of_fibonacci_numbers-2]
-        next_fibonacci_number = f_n_minus_one + f_n_minus_two
+        next_fibonacci_number = self.get_next_fibonacci_number()
         if(next_fibonacci_number < limit):
             if(self.is_even_number(next_fibonacci_number)):
                 self.even_fibonacci_terms.append(next_fibonacci_number)
@@ -29,6 +23,12 @@ class EvenFibonaccyNumbers:
             self.fibonacci_terms.append(next_fibonacci_number)
             self.get_even_terms_below(limit)
         return self.even_fibonacci_terms
+
+    def get_next_fibonacci_number(self):
+        length_of_fibonacci_numbers = len(self.fibonacci_terms)
+        f_n_minus_one = self.fibonacci_terms[length_of_fibonacci_numbers-1]
+        f_n_minus_two = self.fibonacci_terms[length_of_fibonacci_numbers-2]
+        return f_n_minus_one + f_n_minus_two
 
     def get_sum_of_even_fibonacci_terms_below(self, limit):
         even_terms = self.get_even_terms_below(limit)
